d2af9f80094f9bfa77eb6b101325eab795de72d6
Tim Smith <tsmith84@gmail.com>
2015-09-08 12:08:39 -0700

    Add more platforms and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,13 @@ description      "Creates apache2 vhost and serves a kickstart file."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.0"
 depends "apache2"
-supports "redhat"
+
+%w(amazon centos fedora oracle redhat scientific).each do |os|
+  supports os
+end
+
 supports "centos"
 recipe "kickstart", "Empty, use one of the other recipes"
 recipe "kickstart::server", "Sets up kickstart as an apache vhost"
+source_url 'https://github.com/chef-cookbooks/kickstart' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/kickstart/issues' if respond_to?(:issues_url)
