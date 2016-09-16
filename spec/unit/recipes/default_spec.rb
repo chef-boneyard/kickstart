a5187467e7949ec63f08b73467e98e2c9a96a60f
Tim Smith <tsmith@chef.io>
2016-09-16 10:11:33 -0700

    Improve spec
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
-describe 'default recipe on Ubuntu 14.04' do
+describe 'default recipe' do
   let(:chef_run) do
-    ChefSpec::ServerRunner.new do |node|
-      node.automatic[:lsb][:codename] = 'trusty'
-    end.converge('kickstart::default')
+    runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+    runner.converge('kickstart::default')
   end
 
   it 'converges successfully' do
-    expect { :chef_run }.to_not raise_error
+    expect { chef_run }.to_not raise_error
   end
 end
