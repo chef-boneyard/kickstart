865aaa4456f21fb1e5767d1dc89780406aab053f
Tim Smith <tsmith@chef.io>
2017-03-20 12:36:55 -0700

    Use the standard apache-2.0 string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'kickstart'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Creates apache2 vhost and serves a kickstart file.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
