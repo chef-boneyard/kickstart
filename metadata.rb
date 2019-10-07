67b5a4672de6ba56a6792d65dee34ced6a03b82e
Tim Smith <tsmith84@gmail.com>
2019-10-07 16:26:41 -0700

    Resolve cookstyle 5.8 warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,15 +3,12 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Creates apache2 vhost and serves a kickstart file.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 depends          'apache2'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
   supports os
 end
-
-recipe 'kickstart::default', 'Sets up kickstart as an apache vhost'
 
 source_url   'https://github.com/chef-cookbooks/kickstart'
 issues_url   'https://github.com/chef-cookbooks/kickstart/issues'
